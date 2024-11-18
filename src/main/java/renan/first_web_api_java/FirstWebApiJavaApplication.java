d258579fe0991ccd2eb93e2b53d2b567f36e236d
Renan Cunha <renancdev@gmail.com>
2024-11-18 20:21:29 -0300

    Initial commit

---
+++ src/main/java/renan/first_web_api_java/FirstWebApiJavaApplication.java
@@ -0,0 +1,13 @@
+package renan.first_web_api_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWebApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstWebApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/renan/first_web_api_java/FirstWebApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package renan.first_web_api_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWebApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
